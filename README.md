2cfe221a770d8e1a5d2c813d536f7ba2189f360f
kaltun-dev <kaltun19932014@gmail.com>
2022-11-01 23:24:15 +0000

    feat: declared most functions

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ problems and bugs
 problem one 
 array sytaz was wrong and it was returning 250 errors 
 fix: fixed array sysntax problem, it was missing = operator at the begining"
+
+
+problem 2
+questions werent showing on the questions container.
+
+solution, 
+iwas using getelmentbyid eventhough i didnt use id selctor but a class selector.
+
+
+problem 2 
+
+answers buttons and overflowing the center div container.
--- assets/script.js
+++ assets/script.js
@@ -4,9 +4,14 @@ const questionsContainerElement = document.getElementById('question-container')
 
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
+
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 
 function startGame() {
@@ -38,16 +43,43 @@ function showQuestion(question) {
 }
 
 function resetState(){
+    clearStatusClass(document.body)
     nextButton.classList.add('hide')
-    while(answerButtonsElement.firstChild){
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
     }
     
 }
-function selectAnswer(){
-    
+function selectAnswer(e){
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body,correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1 ) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+   
 }
 
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
 const questions = [
     {
         question: 'who founded LinkedIn?',
